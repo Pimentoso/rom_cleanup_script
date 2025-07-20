692dbb78ec1b1388a807743cb8f4b2eafebf5e42
Michele Ferri <ferrimiche@gmail.com>
2025-07-20 11:26:00 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# rom_cleanup_script
-Ruby script to cleanup bad/unwanted ROMs, mainly from TOSEC dumps
+# ROM Cleanup Script
+Ruby script to cleanup bad/unwanted ROMs, mainly from TOSEC dumps.
+
+## How to use
+
+Usage:
+
+```
+ruby rom_cleanup.rb [directory] [.extension]
+```
+
+This is the flow I usually use.
+
+- Download a rom folder from myrient or other sources using `wget -m -np -c -e robots=off -R "index.html*" https://myrient.erista.me/files/TOSEC/...`
+- Move all the roms to a reasonably named folder
+- Run the script using `ruby rom_cleanup.rb /path/to/roms .zip`. The script will remove unwanted ROMs using this criteria
+  - ROMs with bad tags 
+  - ROMs for countries that are not EU or US
+  - ROMs in alpha/beta/prototype state
+  - ROMs with multiple alternate versions (namely [a] tags) - it will only keep the one with the highest number
+  - Then, it will prompt the user for any ROM with multiple region versions (usually EU or US), and the user has to choose which one to keep. I usually keep the US version
+- Manually go through the game directory to see if the script missed something and still have some duplicates
+- Run Retropie or any retrgaming platform you are using, and use the scraper function to get the game metadata. Then manually go through the games and check again if there are duplicate games: some games have a different name between EU and US so scraping the titles should reveal those.
+- Enjoy your curated list of ROMs!
---
+++ rom_cleanup.rb
@@ -0,0 +1,160 @@
+# frozen_string_literal: true
+
+# Usage: ruby rom_cleanup.rb [directory] [.extension]
+# Example: ruby rom_cleanup.rb /path/to/roms .zip
+
+require "logger"
+
+logger = Logger.new($stdout)
+
+DRY_RUN = false
+
+directory = ARGV[0] || "."
+extension = if ARGV[1]&.start_with?(".")
+  ARGV[1]
+else
+  ".zip"
+end
+
+DUMP_INFO_FLAGS = [
+  "cr",
+  "f",
+  "h",
+  "m",
+  # "p",
+  "t",
+  "tr ",
+  "o",
+  "u",
+  "v",
+  "b",
+  "a " # weird alternate versions
+]
+
+COUNTRIES = %w[DE FR ES IT SE BR RU AU CA JP TW KR CN AS JP-KR]
+
+DEV_STATUS = [
+  "alpha",
+  "beta",
+  "pre-release",
+  "proto",
+  "demo"
+]
+
+if DRY_RUN
+  logger.info { "##############################################" }
+  logger.info { "##### DRY RUN - no files will be deleted #####" }
+  logger.info { "##############################################" }
+  logger.info { "" }
+end
+
+logger.info { "################ REMOVING ROMS BY UNWANTED FLAGS ################" }
+
+files = Dir.glob(File.join(directory, "*#{extension}"))
+files.each do |file|
+  deleted = nil
+
+  DUMP_INFO_FLAGS.each do |flag|
+    if file.include?("[#{flag}")
+      logger.debug { "contains [#{flag}] - deleting: #{file}" }
+      deleted = File.delete(file) unless DRY_RUN
+      break
+    end
+  end
+  next if deleted
+
+  COUNTRIES.each do |code|
+    if file.include?("(#{code})")
+      logger.debug { "contains (#{code}) - deleting: #{file}" }
+      deleted = File.delete(file) unless DRY_RUN
+      break
+    end
+  end
+  next if deleted
+
+  DEV_STATUS.each do |flag|
+    if file.include?("(#{flag}")
+      logger.debug { "contains (#{flag}) - deleting: #{file}" }
+      deleted = File.delete(file) unless DRY_RUN
+      break
+    end
+  end
+  next if deleted
+end
+
+logger.info { "Done." }
+logger.info { "################ REMOVING ROMS WITH MULTIPLE ALTERNATE VERSIONS ################" }
+
+base_names = Dir.glob(File.join(directory, "*#{extension}")).reject { |f| f =~ /\[a/ }
+
+base_names.each do |file|
+  basename = File.basename(file, extension)
+
+  matches = Dir.glob(File.join(directory, "#{basename}*")).select { |f| f =~ /\[a/ }.sort
+  next unless matches.any?
+
+  match_to_keep = nil
+  max_a = 0
+  matches.each do |match|
+    a = if match.include?("[a]")
+      1
+    elsif rev = match.match(/\[a(\d*)\]/)
+      rev[1].to_i
+    else
+      0
+    end
+
+    if a > max_a
+      max_a = a
+      match_to_keep = match
+    end
+  end
+
+  if match_to_keep
+    logger.debug { "Found latest alternate version: #{match_to_keep}" }
+
+    matches.each do |match|
+      if match != match_to_keep
+        logger.debug { "Deleting old version: #{match}" }
+        File.delete(match) unless DRY_RUN
+      end
+    end
+    logger.debug { "Deleting old version: #{file}" }
+    File.delete(file) unless DRY_RUN
+  end
+end
+
+logger.info { "Done." }
+logger.info { "################ REMOVING ROMS WITH MULTIPLE REGION (user input required) ################" }
+
+base_names = Dir.glob(File.join(directory, "*#{extension}")).map { |f| File.basename(f, extension).split("(").first.strip }.uniq
+
+base_names.each do |file|
+  basename = "#{file} ("
+  matches = Dir.glob(File.join(directory, "#{basename}*")).sort
+  next unless matches.size > 1
+
+  logger.info { "Multiple versions found for: #{file}" }
+  matches.each_with_index do |match, index|
+    logger.info { "#{index + 1}. #{File.basename(match)}" }
+  end
+
+  logger.info { "Enter number to keep (1-#{matches.size}): " }
+  selection = STDIN.gets.chomp.to_i
+
+  if selection.between?(1, matches.size)
+    file_to_keep = matches[selection - 1]
+    matches.each do |match|
+      if match != file_to_keep
+        logger.debug { "deleting: #{match}" }
+        File.delete(match) unless DRY_RUN
+      end
+    end
+  else
+    logger.info { "Invalid selection, skipping..." }
+  end
+
+  logger.info { "################" }
+end
+
+logger.info { "Done." }
