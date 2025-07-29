f391d01fd3ccda27708333ede696a55ef63726ae
Michele Ferri <ferrimiche@gmail.com>
2025-07-29 15:06:24 +0200

    add multiple revision removal

--- rom_cleanup.rb
+++ rom_cleanup.rb
@@ -128,6 +128,48 @@ base_names.each do |file|
 end
 
 logger.info { "Done." }
+logger.info { "################ REMOVING ROMS WITH MULTIPLE REVISIONS (user input required) ################" }
+
+base_names = Dir.glob(File.join(directory, "*#{extension}")).select{ |f| f.include?("Rev ") }.map { |f| File.basename(f, extension).split("Rev ").first.strip }.uniq
+
+base_names.each do |file|
+  matches = [Dir.glob(File.join(directory, "#{file} Rev *")), Dir.glob(File.join(directory, "#{file} ("))].flatten.sort
+  if matches.size == 1
+    file_to_keep = matches[0]
+    new_filename = File.join(directory, "#{file} #{file_to_keep[file_to_keep.index('(')..]}")
+    logger.debug { "renaming #{file_to_keep} to #{new_filename}" }
+    File.rename(file_to_keep, new_filename) unless DRY_RUN
+
+    next
+  end
+
+  logger.info { "Multiple versions found for: #{file}" }
+  matches.each_with_index do |match, index|
+    logger.info { "#{index + 1}. #{File.basename(match)}" }
+  end
+
+  logger.info { "Enter number to keep (1-#{matches.size}): " }
+  selection = STDIN.gets.chomp.to_i
+
+  if selection.between?(1, matches.size)
+    file_to_keep = matches[selection - 1]
+    matches.each do |match|
+      if match != file_to_keep
+        logger.debug { "deleting: #{match}" }
+        File.delete(match) unless DRY_RUN
+      end
+    end
+    new_filename = File.join(directory, "#{file} #{file_to_keep[file_to_keep.index('(')..]}")
+    logger.debug { "renaming #{file_to_keep} to #{new_filename}" }
+    File.rename(file_to_keep, new_filename) unless DRY_RUN
+  else
+    logger.info { "Invalid selection, skipping..." }
+  end
+
+  logger.info { "################" }
+end
+
+logger.info { "Done." }
 logger.info { "################ REMOVING ROMS WITH MULTIPLE REGION (user input required) ################" }
 
 base_names = Dir.glob(File.join(directory, "*#{extension}")).map { |f| File.basename(f, extension).split("(").first.strip }.uniq
